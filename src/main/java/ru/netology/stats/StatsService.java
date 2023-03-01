44c15ae4088aaa91018a20ac16aca1384d3f1851
MaratIsashev <116102757+MaratIsashev@users.noreply.github.com>
2023-03-01 23:24:00 +0300

    Update StatsService.java

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -53,7 +53,6 @@ public class StatsService {
             if (sale > average) {
                 aboveAverageCounter++;
             }
-            ;
         }
         return aboveAverageCounter;
     }
@@ -69,7 +68,6 @@ public class StatsService {
             if (sale < average) {
                 belowAverageCounter++;
             }
-            ;
         }
         return belowAverageCounter;
     }
